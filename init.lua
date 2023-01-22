c139230c44b62555c8f15f69acea8f7284a86721
Gregory Power <gregorywpower@gmail.com>
2023-01-22 02:23:08 -0500

    post zsh fixing

--- init.lua
+++ init.lua
@@ -30,7 +30,7 @@ require('packer').startup(function(use)
     'hrsh7th/nvim-cmp',
     requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
   }
-    
+
   -- Adds a whole history of changes made to a file.
   use 'mbbill/undotree'
 
