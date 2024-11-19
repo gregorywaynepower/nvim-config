429970bc594c9527eb85f23dd7da17c603d993e2
Gregory Power <31050507+gregorywaynepower@users.noreply.github.com>
2024-11-19 01:56:49 -0500

    add support for pyright and python autocomplete

--- init.lua
+++ init.lua
@@ -592,7 +592,8 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        -- ruff = {},
+        pyright = { disableOrganizeImports = true },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
