24abb67645f3a8e9a1dd35c63eed5a6cc13c22e8
Gregory Power <31050507+gregorywaynepower@users.noreply.github.com>
2024-11-19 02:01:02 -0500

    add config from astral-sh/ruff-lsp repo for pyright

--- init.lua
+++ init.lua
@@ -593,7 +593,7 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         -- ruff = {},
-        pyright = { disableOrganizeImports = true },
+        pyright = { disableOrganizeImports = true, python = { analysis = { ignore = '*' } } },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
